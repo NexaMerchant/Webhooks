dfcab3ef40e5b93be20c959d0917e4d18f362b33
Steve <34465153+xxl4@users.noreply.github.com>
2024-11-19 18:25:07 +0800

    add webhook v1

---
+++ src/Http/Controllers/Api/WebhookController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace NexaMerchant\Webhooks\Http\Controllers\Api;
+
+use Illuminate\Foundation\Validation\ValidatesRequests;
+
+class WebhookController extends Controller
+{
+    // create a new webhook
+
+    // update a webhook
+
+    // delete a webhook
+
+    // list all webhooks
+
+    // get a webhook
+
+    // test a webhook
+}
--- src/Routes/api.php
+++ src/Routes/api.php
@@ -1,14 +1,27 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use NexaMerchant\Webhooks\Http\Controllers\Api\ExampleController;
+use NexaMerchant\Webhooks\Http\Controllers\Api\WebhookController;
 
 Route::group(['middleware' => ['api'], 'prefix' => 'api'], function () {
-     Route::prefix('webhooks')->group(function () {
+    // v1 webhooks
+     Route::prefix('v1')->group(function () {
 
-        Route::controller(ExampleController::class)->prefix('example')->group(function () {
+        Route::controller(WebhookController::class)->prefix('webhook')->group(function () {
 
-            Route::get('demo', 'demo')->name('webhooks.api.example.demo');
+            // Route::get('demo', 'demo')->name('webhooks.api.example.demo');
+
+            Route::get('list', 'list')->name('webhooks.api.webhook.list');
+
+            Route::get('get/{id}', 'get')->name('webhooks.api.webhook.get');
+
+            Route::post('create', 'create')->name('webhooks.api.webhook.create');
+
+            Route::put('update/{id}', 'update')->name('webhooks.api.webhook.update');
+
+            Route::delete('delete/{id}', 'delete')->name('webhooks.api.webhook.delete');
+
+            Route::post('test/{id}', 'test')->name('webhooks.api.webhook.test');
 
         });
 
